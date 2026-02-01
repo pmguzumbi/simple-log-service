b8e03af6b41aea56fdb5a69af87019f9516647b8
pmguzumbi <143409778+pmguzumbi@users.noreply.github.com>
2026-02-01 11:34:27 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,12 +42,6 @@ provider "aws" {
   }
 }
 
-# Data source for current AWS account ID
-data "aws_caller_identity" "current" {}
-
-# Data source for current AWS region
-data "aws_region" "current" {}
-
 # Data source for available availability zones
 data "aws_availability_zones" "available" {
   state = "available"
@@ -76,3 +70,4 @@ locals {
     ManagedBy   = "Terraform"
   }
 }
+
